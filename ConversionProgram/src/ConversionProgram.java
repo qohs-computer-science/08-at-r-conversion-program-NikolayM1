e3c8f855ef27959b9734146bc723437e53c0941f
Nikolay <nmerenko07@gmail.com>
2025-09-18 16:46:04 +0000

    Started Program. Changed original file name.

---
+++ ConversionProgram/src/ConversionProgram.java
@@ -0,0 +1,18 @@
+/*
+ * Nikolay Merenko
+ * September 18th, 2025
+ * Period 6
+ * A program the converts a user-inputted number from decimal or binary, or vice versa, depending on the user's preference.
+ */
+import java.util.Scanner; 
+
+public class ConversionProgram {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Conversion Program! Please select an option:");
+		System.out.println("1. Convert Decimal to Binary");
+		System.out.println("2. Convert Binary to Decimal");
+		int selection = in.nextInt(); 
+	}
+
+}
