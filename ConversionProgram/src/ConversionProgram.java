47cd23a0d2eb098343f15d0c2b0870197113a930
Nikolay <nmerenko07@gmail.com>
2025-09-20 03:25:20 +0000

    Ready for Grading.

--- ConversionProgram/src/ConversionProgram.java
+++ ConversionProgram/src/ConversionProgram.java
@@ -1,17 +1,18 @@
 /*
  * Nikolay Merenko
- * September 18th, 2025
+ * September 19th, 2025
  * Period 6
  * A program the converts a user-inputted number from decimal or binary, or vice versa, depending on the user's preference.
  */
 
-//REMINDER: ADD COMMENTS
-import java.util.Scanner; 
+import java.util.Scanner; //scanner is imported
 
-public class ConversionProgram {
+public class ConversionProgram { //class begins
 
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
+	public static void main(String[] args) { //method begins
+		Scanner in = new Scanner(System.in);  //scanner 
+
+		while (true) { //while loop makes sure you can continue later
 		System.out.println("Welcome to the Conversion Program! Please select an option:");
 		System.out.println("1. Convert Decimal to Binary");
 		System.out.println("2. Convert Binary to Decimal");
@@ -20,29 +21,30 @@ public class ConversionProgram {
 		if (selection == 1) {
 			System.out.print("You have chosen to convert from decimal to binary. Enter a decimal number: ");
 			int decimal = in.nextInt();
-			String binary = Integer.toBinaryString(decimal);
+			String binary = Integer.toBinaryString(decimal); // this method converts decimal to binary (found w/ independent research)
 			System.out.println("The binary representation of your inputted number is: " + binary);
 		} else if (selection == 2) {
 			System.out.print("You have chosen to convert from binary to decimal. Enter a binary number: ");
 			String binary = in.next();
-			int decimal = Integer.parseInt(binary, 2);
+			int decimal = Integer.parseInt(binary, 2); //automatically converts binary to decimal
 			System.out.println("The decimal representation is: " + decimal);
 		} else {
-			System.out.println("That doesn't work. Please choose either 1 or 2.");
+			System.out.println("That doesn't work. Please choose either 1 or 2."); //checks for errors
 			return;
 		}
+		
+ 		System.out.print("Would you like to convert another number? Answer yes or no.");
+            String again = in.next();
 
-		System.out.println("Would you like to continue? Please enter Y for yes or N for no.");
-		String continuation = in.nextLine();
+            if (!again.equalsIgnoreCase("yes")) { //if the user doesn't want to continue, the program ends (had to search for a way to do this)
+                System.out.println("Thank you for using the Conversion Program!");
+                break; 
+            }
+            
+        } //while loop ends
 
-		if (continuation == "Y") {
-			main(null);
-		} else if (continuation == "N") {
-			System.out.println("Thank you for using the Conversion Program!");
-		} else {					
-			System.out.println("Your input is invalid. Please enter either Y or N.");	//fix this - it should not pop up after the user inputs the binary or decimal number
-			return;
-		}
+		} //methods ends
 
-	}
-}
+	} //class ends
+
+	//program ends
